fa76a79157f93a687b937e52a16f3f2692f13ea5
mr-d-luffy <zoroxpro@proton.me>
2025-01-30 19:59:36 +0530

    Hello

---
+++ src/main.py
@@ -0,0 +1,218 @@
+import json
+from tkinter import *
+from colorama import Fore
+from pygame import mixer
+from os import system
+from module.clinic import clinic, clinicDocument
+
+api = json.load(open("json/data.json")) # load the json file in project 
+backend = clinic("clinic") # backend module in clinic file
+documentBackend = clinicDocument() # function for create bill in document format(PDF)
+
+# search data in database using this gui
+class DatabaseWindow(Tk):
+    # search data by id in this function 
+    def databaseCustomerId(self):
+        lx1 = Label(self, text="Customer ID : ", fg="white", bg="black")
+        lx1.place(x=10,y=20)
+
+        customerId = Entry(self, fg="white", bg="black")
+        customerId.place(x=110, y=18)
+
+        def searchData():
+            cusid = customerId.get()
+            backend.showDataById(cusid)
+            exit()
+
+        bx1 = Button(self, text="searchByID", fg="black", bg="white", command=searchData)
+        bx1.place(x=170, y=70)
+
+    # search data by number in this function
+    def databaseCustomerNumber(self):
+        lx2 = Label(self, text="CustomerNumber : ", fg="white", bg="black")
+        lx2.place(x=10,y=20)
+
+        customerNumber = Entry(self, fg="white", bg="black")
+        customerNumber.place(x=130, y=18)
+
+        def searchData():
+            cusNumber = customerNumber.get()
+            backend.showDataByNumber(cusNumber)
+            exit()
+
+        bx2 = Button(self, text="searchByNumber", fg="black", bg="white", command=searchData)
+        bx2.place(x=150, y=70)
+
+
+# this is for new window 
+class customerIDWindow(DatabaseWindow):
+    #delete data from database using this function
+    def windowDelete(self):
+        lx = Label(self, text="Customer ID : ", fg="white", bg="black")
+        lx.place(x=10,y=20)
+
+        customerId = Entry(self, fg="white", bg="black")
+        customerId.place(x=110, y=18)
+
+        def deleteData():
+            cusid = customerId.get()
+            print(cusid)
+            backend.deleteDataById(cusid)
+            exit()
+
+        bx = Button(self, text="Delete", fg="black", bg="white", command=deleteData)
+        bx.place(x=170, y=70)
+
+class main(customerIDWindow):
+    def __ini__(self):
+        self.name = self.customerName.get()
+        self.age = self.customerage.get()
+        self.cusId = self.backend.createID() 
+        self.gender = self.selectMenu.get()
+        self.number = self.contactNumber.get()
+        self.test = self.customerTest.get()
+        self.fees = self.customerFees.get()
+
+    # this function is a create GUI window by using tkinter module 
+    def App(self):
+        # gui for input a customer name 
+        l1 = Label(text="Customer Name : ", fg="white", bg="black")
+        l1.place(x=10, y=20)
+        customerName = Entry(fg="white", bg="black")
+        customerName.place(x=130, y=16)
+
+        # gui for input customer age
+        l2 = Label(text="age : ", fg="white", bg="black")
+        l2.place(x=335, y=20)
+        customerage = Entry(fg="white", bg="black")
+        customerage.place(x=390, y=16)
+
+        # gui for input customer gender
+        l3 = Label(text="Gender : ", fg="white", bg="black")
+        l3.place(x=30, y=60)
+        selectMenu = StringVar()
+        selectMenu.set("Choose gender")
+        dm1 = OptionMenu(self, selectMenu, *api["gender"])
+        dm1.place(x=90, y=60)
+
+        # gui for input customer contact number 
+        l4 = Label(text="Contact : ", fg="white", bg="black")
+        l4.place(x=255, y=60)
+        contactNumber = Entry(fg="white", bg="black")
+        contactNumber.place(x=330, y=56)
+
+        # gui for input clinical tests
+        l5 = Label(text="Test : ", fg="white", bg="black")
+        l5.place(x=35, y=110)
+        customerTest = Entry(fg="white", bg="black")
+        customerTest.place(x=90, y=108)
+
+        # gui for input test fees
+        l6 = Label(text="Fees : ", fg="white", bg="black")
+        l6.place(x=300, y=110)
+        customerFees = Entry(fg="white", bg="black")
+        customerFees.place(x=350, y=108)
+
+        # this function for onclick event in windows (for submit a data in sqlite database)
+        def update():
+            data = {
+                "CustomerName":self.name,
+                "Customerself.age":self.age,
+                "CustomerId":self.cusId,
+                "Gender":self.gender,
+                "ConatactNumber":self.number,
+                "Test":self.test,
+                "Fees":self.fees   
+            }
+            print(self.name, self.age, self.cusId, self.gender, self.number, self.test, self.fees)
+            backend.createQrcode(data, self.cusId) # this is for make qrcode in this path:qrcode/FileName.png
+            binaryImage = backend.imself.ageToBinary(self.cusId) # convert imself.age into binary format
+            backend.storeData(self.name, self.number, self.cusId, self.test, self.age, self.gender, self.fees, binaryImage) #store data in database using this funtion
+            backend.readDatabase() #reading the database using this function
+            App.destroy()
+
+        # gui for submit data to the database
+        bt1 = Button(text="Submit", fg="black", bg="white", command=update)
+        bt1.place(x=100, y=200)
+
+        def documentPrint():
+            documentBackend.printDocument(self.name, self.age, self.gender, self.number, self.cusId, self.test, self.fees)
+
+        # print PDF in document page
+        bt2 = Button(text="print", fg="black", bg="white", command=documentPrint)
+        bt2.place(x=200, y=200)
+
+        def showDataByID():
+            databaseApp = DatabaseWindow()
+            databaseApp.databaseCustomerId()
+            databaseApp.title(api["company"])
+            databaseApp.geometry("400x125")
+            databaseApp.configure(bg=api["background"])
+            databaseApp.resizable(api["windowSize"][0], api["windowSize"][0])
+            databaseApp.mainloop()
+            
+
+        # show data from database
+        bt3 = Button(text="searchDataById", fg="black", bg="white", command=showDataByID)
+        bt3.place(x=300, y=200)
+
+        def showDataByNumber():
+            databaseApp = DatabaseWindow()
+            databaseApp.databaseCustomerNumber()
+            databaseApp.title(api["company"])
+            databaseApp.geometry("400x125")
+            databaseApp.configure(bg=api["background"])
+            databaseApp.resizable(api["windowSize"][0], api["windowSize"][0])
+            databaseApp.mainloop()
+            
+
+        # show data from database
+        bt3 = Button(text="searchDataByNumber", fg="black", bg="white", command=showDataByNumber)
+        bt3.place(x=300, y=230)
+
+        # making a new window for delete customer data from database 
+        def deleteRecord():
+            deleteWindow = customerIDWindow()
+            deleteWindow.windowDelete()
+            deleteWindow.title(api["company"])
+            deleteWindow.geometry("400x125")
+            deleteWindow.configure(bg=api["background"])
+            deleteWindow.resizable(api["windowSize"][0], api["windowSize"][0])
+            deleteWindow = mainloop()
+
+        # delete data button
+        bt4 = Button(text="deleteData", fg="black", bg="white", command=deleteRecord)
+        bt4.place(x=425, y=200)
+        
+        closeWindow = lambda : App.destroy()
+
+        # close the window
+        bt5 = Button(text="close", fg="black", bg="white", command=closeWindow)
+        bt5.place(x=535, y=200)
+
+# this code run on only this file (you can import this module in other file this code can not run)
+if __name__== "__main__":
+    system("clear") # clean the terminal for better view
+    App = main() # creating object for main window
+    App.App() # Run App
+    App.title(api["company"]) # set title in main window
+    App.geometry("650x350") # set size of main window
+    App.configure(bg=api["background"]) # background color of main window
+    App.resizable(api["windowSize"][1], api["windowSize"][1]) # set window resize True or False 
+
+    # set the application background music using this code
+    try:
+        mixer.init()
+        mixer.music.load(open(api["path"][4]))
+        mixer.music.play()
+    except(Exception) as e:
+        print(Fore.RED,e,Fore.WHITE)
+
+    # set the application icon using this code
+    try:
+        logo = PhotoImage(file=api["path"][3])
+        App.iconphoto(False, logo)
+    except(Exception) as e:
+        print(Fore.RED,e,Fore.WHITE)
+        
+    App.mainloop()
---
+++ src/module/clinic.py
@@ -0,0 +1,134 @@
+from json import load
+from colorama import Fore
+from random import sample
+from datetime import datetime
+import pandas as pd
+from qrcode import make
+from PIL import Image
+from fpdf import FPDF
+import sqlite3
+
+api = load(open("json/data.json")) # load the json file in project
+
+class clinic:
+    #this constructor created for input filename and Connect to the database
+    def __init__(self, tableName):
+        self.connection = sqlite3.connect(api["path"][2])
+        self.c = self.connection.cursor()
+        self.tableName = tableName
+    
+    # create table in database by this function
+    def createDatabase(self):
+        try:
+            self.c.execute(f"CREATE TABLE {self.tableName} (dateTime text, name text, contactNumber text, customerID text, test text, age text, gender text, fees text, qrcodeimage blob)")
+            self.connection.commit()
+        except(sqlite3.Error) as e:
+            print(Fore.RED,f"Database already connected {e}",Fore.WHITE)
+
+    # this function create 12 digit id for searsh in database
+    def createID(self):
+        number = "1234567890"
+        alpha = "abcdefghijklmnoqrstuvwxyz"
+        all = number+alpha
+        ID = ''.join(sample(all, 12))
+        return ID
+    
+    # store data in database using this function
+    def storeData(self, name, contactNumber, customerID, test, age, gender, fees, bynaryQR):
+        date = datetime.now()
+        try:
+            self.c.execute(f"INSERT INTO {self.tableName} VALUES (?,?,?,?,?,?,?,?,?)", (date, name, contactNumber, customerID, test, age, gender, fees, bynaryQR))
+            self.connection.commit()
+        except(sqlite3.Error) as e:
+            print(Fore.RED,e,Fore.WHITE)
+
+    # show data from database by id using this function
+    def showDataById(self, customerID):
+        try:
+            self.c.execute(f"SELCET * FROM {self.tableName} where customerID={customerID}")
+            self.c.fetchall()
+            self.connection.commit()
+            self.connection.close()
+        except(sqlite3.Error) as e:
+            print(Fore.RED,e,Fore.WHITE)
+
+    # show data from database by number using this function
+    def showDataByNumber(self, contactNumber):
+        try:
+            self.c.execute(f"SELECT * FROM {self.tableName} where contactNumber={contactNumber}")
+            self.c.fetchall()
+            self.connection.commit()
+            self.connection.close()
+        except(sqlite3.Error) as e:
+            print(Fore.RED,e,Fore.WHITE)
+
+    # delete data from database using this function
+    def deleteDataById(self, customerID):
+        try:
+            self.c.execute(f"DELETE * FROM {self.tableName} WHERE customerID={customerID} ")
+            self.connection.commit()
+            self.connection.close()
+        except(sqlite3.Error) as e:
+            print(Fore.RED,e,Fore.WHITE)
+
+    # read database using this function
+    def readDatabase(self):
+        try:
+            query = f"SELECT * FROM {self.tableName}"
+            data = pd.read_sql_query(query, self.connection)
+            print(data)
+        except(Exception) as e:
+            print(Fore.RED, e, Fore.WHITE)
+
+    #this function for create qrcode from the input data
+    def createQrcode(self, data, fileName):
+        try:
+            image = make(data)
+            image.save(f"{api["path"][5]} {fileName}.png")
+            Photo = Image.open(f"{api["path"][5]} {fileName}.png")
+            Photo.show() # open qrcode in new window using this function
+        except(Exception) as e:
+            print(Fore.RED, e, Fore.WHITE)
+
+    # Canvert image into binary Form to store in database
+    def imageToBinary(self, image):
+        try:
+            with open(f"{api["path"][5]}{image}.png", "rb") as File:
+                data = File.read()
+                File.close()
+            return data
+        except(Exception) as e:
+            print(Fore.RED,e,Fore.WHITE)
+
+# this class for make data into dcoument format(PDF)
+class clinicDocument(FPDF, clinic) :
+    # get the output print in PDF format using this function
+    def printDocument(self, name, age, gender, number, customerid, test, fees):
+        try:
+            self.add_page()
+            self.set_font("Arial", "IB", 12)
+            self.ln(1)
+            self.cell(0,10, f"Name : {name}")
+            self.ln(2)
+            self.cell(0,20, f"Age : {age}")
+            self.ln(3)
+            self.cell(0,30, f"Gender : {gender}")
+            self.ln(4)
+            self.cell(0,40, f"Number : {number}")
+            self.ln(5)
+            self.cell(0,50, f"ID : {customerid}")
+            self.ln(6)
+            self.cell(0,60, f"test : {test}")
+            self.ln(7)
+            self.cell(0,70, f"fees : {fees}")
+            self.ln(8)
+            self.output(f"{api["path"][6]}{customerid}.pdf")
+        except(Exception) as e:
+            print(Fore.RED, e, Fore.WHITE)
+
+if __name__=="__main__":
+    c = clinic("clinic")
+    cd = clinicDocument()
+    # cd.printDocument("kali", 19, "male", 8788358100, "mkh98h9g0", "blood", 250)
+    
+    
